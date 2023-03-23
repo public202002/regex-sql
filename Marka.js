7a4e70649c8dd505377ca1fdae7657f6936f4947
public202002 <77833368+public202002@users.noreply.github.com>
2023-03-23 23:08:56 +0000

    Pending changes exported from your codespace

---
+++ Marka.js
@@ -0,0 +1,3 @@
+
+var dog = 3+5
+console.log("Hello cat!", 1+1, dog)
--- relation.mjs
+++ relation.mjs
@@ -12,9 +12,53 @@ var r = Regex.new()
     .add(/\((?<PK>.+?)\)/)
     .add(' on '.replaceAll(" ", "\\s*"))
     .add(''.replaceAll(" ", "\\s*"))
-    .flags("gm")
+    .flags("m")
     .build();  
-    console.log("as string: \[ " + r)
+    console.log("r as string: ~~~" + r)
+    console.log("~~~\r\n ")
+    
 var source = "Alter table [haha].[haha].[sRISRESPPol] add Constraint [FK_cUcZnak_sRISRESPPol] foreign key([IdUcZnak],[IfUcZnak],[huhu]) references [dbo].[cUcZnak] ([Id],[FirstId])  on "
+source = "Alter table [uRozpUpravaPol] add Constraint [FK_cAkce_uRozpUpravaPol] foreign key([IdAkce],[IfAkce]) references [dbo].[cAkce] ([Id],[FirstId])  on update no action on delete no action "
+source = "Alter table [dbo].[cOrgRok] add Constraint [FK_cOrg_cOrgRok] foreign key([IdOrg],[IfOrg]) references [dbo].[cOrg] ([Id],[FirstId])  on update no action on delete cascade "
 var m = r.exec(source)
-console.log("as match:", m ) 
+//console.log("as match:", m )  
+
+const regexRelation = Regex.new()
+    .add('Alter table '.replaceAll(" ", "\\s*"))
+    .add(/(?:\[.+?\].)?(?:\[.+?\].)?(?:\[(?<foreginTable>.+?)\])/)
+    .add(' add Constraint '.replaceAll(" ", "\\s*"))
+    .add(/\[.+?\]/)
+    .add(' foreign key'.replaceAll(" ", "\\s*"))
+    .add(/\((?<FK>.+?)\)/)
+    .add(' references '.replaceAll(" ", "\\s*"))
+    .add(/(?:\[.+?\].)?(?:\[.+?\].)?(?:\[(?<primaryTable>.+?)\])/)
+    .add(' '.replaceAll(" ", "\\s*"))
+    .add(/\((?<PK>.+?)\)/)
+    .add(' on '.replaceAll(" ", "\\s*"))
+    .add(''.replaceAll(" ", "\\s*"))
+    .flags("m")
+    .build();
+    console.log("regexRelation as string: ~~~" + regexRelation)
+    console.log("~~~\r\n ")
+    
+    m = regexRelation.exec(source);
+    if (!!(m?.groups)) {
+        console.log("as match2:", m )  
+    }else{
+        console.log("as match2X:", m )  
+    }
+
+
+    m = regexRelation.exec(source);
+    if (!!(m?.groups)) {
+        console.log("as match2:", m )  
+    }else{
+        console.log("as match2X:", m )    
+    }
+    m = regexRelation.exec(source);
+    if (!!(m?.groups)) {
+        console.log("as match2:", m )  
+    }else{
+        console.log("as match2X:", m )  
+    }
+        
